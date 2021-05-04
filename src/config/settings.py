27812fba6ad52b3ad0e6e7a380e8a29aec36d99f
xiaoli.dong <xiaolid@gmail.com>
2021-05-03 21:55:56 -0600

    commit

--- src/config/settings.py
+++ src/config/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
